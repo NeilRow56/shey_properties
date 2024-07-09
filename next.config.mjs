d9cfa6f14644611e93b7b0886fa8be55fb220ba9
NeilRow56 <neilrowland56@gmail.com>
2024-07-09 06:24:53 +0100

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+      {
+        protocol: "https",
+        hostname: "a0.muscache.com",
+      },
+      {
+        protocol: "https",
+        hostname: "files.edgestore.dev",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
