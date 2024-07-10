13ed614a7a7f12236fe8d58ce4c273d90cf738b3
NeilRow56 <neilrowland56@gmail.com>
2024-07-10 14:15:01 +0100

    Updated property form colours

--- src/app/site/user/properties/_components/property-form/index.tsx
+++ src/app/site/user/properties/_components/property-form/index.tsx
@@ -50,18 +50,18 @@ export const PropertyForm = () => {
           {steps.map((step, index) => (
             <li key={step.title} className='md:flex-1'>
               {currentStep > index ? (
-                <div className='group flex w-full items-center gap-3 border-l-4 border-sky-600 py-2 pl-4 transition-colors md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4'>
-                  <span className="bg-sky-500 w-[33px] h-[33px] rounded-full items-center justify-center text-[#1B4242] border-slate-100">
+                <div className='group flex w-full items-center gap-3 border-l-4 border-[#1B4242] py-2 pl-4 transition-colors md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4'>
+                  <span className="bg-[#1B4242] w-[33px] h-[33px] rounded-full items-center justify-center text-slate-200 border-slate-100">
                     <div className="text-center pt-1">{step.id}</div>
                   </span>
                   <span className='text-sm font-medium'>{step.title}</span>
                 </div>
               ) : currentStep === index ? (
                 <div
-                  className='flex w-full items-center gap-3 border-l-4 border-sky-600 py-2 pl-4 md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4'
+                  className='flex w-full items-center gap-3 border-l-4 border-[#1B4242] py-2 pl-4 md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4'
                   aria-current='step'
                 >
-                  <span className="bg-sky-500  w-[33px] h-[33px] rounded-full items-center justify-center text-[#] border-slate-100">
+                  <span className="bg-[#1B4242]  w-[33px] h-[33px] rounded-full items-center justify-center text-slate-200 border-slate-100">
                     <div className="text-center pt-1">{step.id}</div>
                   </span>
                   <span className='text-sm font-medium'>{step.title}</span>
@@ -80,7 +80,7 @@ export const PropertyForm = () => {
       </nav>
 
       
-     <div>
+     <div className="mt-8">
       {currentStep === 0 && (
           
           <Basic />
