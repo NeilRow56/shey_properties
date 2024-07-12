4c430a9aad138ad95ea361b8645614ab02690887
NeilRow56 <neilrowland56@gmail.com>
2024-07-12 13:37:20 +0100

    Basic form completed for Property Form

--- src/app/site/user/properties/_components/property-form/basic.tsx
+++ src/app/site/user/properties/_components/property-form/basic.tsx
@@ -1,7 +1,8 @@
 "use client";
 import React from "react";
 import { PropertiesFormStepProps } from ".";
-import { Button } from "antd";
+import { Button, Form, Input, InputNumber, Select } from "antd";
+import { propertyStatuses, propertyTypes } from "@/constants";
 
 export const Basic = ({
   currentStep,
@@ -9,9 +10,80 @@ export const Basic = ({
   finalValues,
   setFinalValues,
 }: PropertiesFormStepProps) => {
+  const onFinish = (values: any) => {
+    setFinalValues({ ...finalValues, basic: values });
+    setCurrentStep(currentStep + 1);
+  };
   return (
-    <div>
-      <span>Basic</span>
+    <Form
+      onFinish={onFinish}
+      layout="vertical"
+      initialValues={finalValues.basic}
+    >
+      <div className="grid grid-cols-1 gap-5 mt-24">
+        <Form.Item
+          name="name"
+          label="Property Name"
+          rules={[
+            {
+              required: true,
+              message: "Please input property name!",
+            },
+          ]}
+        >
+          <Input placeholder="Property Name" className="rounded-md" />
+        </Form.Item>
+        <Form.Item
+          name="description"
+          label="Description"
+          rules={[
+            {
+              required: true,
+              message: "Please input description!",
+            },
+          ]}
+        >
+          <Input.TextArea rows={6} placeholder="Description" />
+        </Form.Item>
+      </div>
+      <div className="grid grid-cols-3 gap-5 mt-5">
+        <Form.Item
+          name="type"
+          label="Type"
+          rules={[
+            {
+              required: true,
+              message: "Please input type!",
+            },
+          ]}
+        >
+          <Select options={propertyTypes} />
+        </Form.Item>
+        <Form.Item
+          name="status"
+          label="Status"
+          rules={[
+            {
+              required: true,
+              message: "Please input status!",
+            },
+          ]}
+        >
+          <Select options={propertyStatuses} />
+        </Form.Item>
+        <Form.Item
+          name="price"
+          label="Price"
+          rules={[
+            {
+              required: true,
+              message: "Please input price!",
+            },
+          ]}
+        >
+          <InputNumber className="w-full" type="number" placeholder="Price" />
+        </Form.Item>
+      </div>
       <div className="flex justify-end gap-5 mt-7">
         <Button
           disabled={currentStep === 0}
@@ -19,10 +91,10 @@ export const Basic = ({
         >
           Back
         </Button>
-        <Button type="primary" onClick={() => setCurrentStep(currentStep + 1)}>
+        <Button htmlType="submit" type="primary">
           Next
         </Button>
       </div>
-    </div>
+    </Form>
   );
 };
