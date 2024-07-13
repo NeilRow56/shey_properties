e531a9ee6fd833f22e1924c12bcfaf9fe8eab8bb
NeilRow56 <neilrowland56@gmail.com>
2024-07-13 05:37:09 +0100

    Added amentities and location sections of property form

--- src/app/site/user/properties/_components/property-form/location.tsx
+++ src/app/site/user/properties/_components/property-form/location.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { PropertiesFormStepProps } from ".";
-import { Button } from "antd";
+import { Button, Input, Form } from "antd";
 
 export const Location = ({
   currentStep,
@@ -8,9 +8,68 @@ export const Location = ({
   finalValues,
   setFinalValues,
 }: PropertiesFormStepProps) => {
+  const onFinish = (values: any) => {
+    setFinalValues({ ...finalValues, location: values });
+    setCurrentStep(currentStep + 1);
+  };
+
   return (
-    <div>
-      <span>Location</span>
+    <Form
+      layout="vertical"
+      onFinish={onFinish}
+      initialValues={finalValues.location}
+    >
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
+        <Form.Item
+          name="town"
+          label="Town"
+          rules={[
+            {
+              required: true,
+              message: "Please input town!",
+            },
+          ]}
+        >
+          <Input placeholder="Town" />
+        </Form.Item>
+        <Form.Item
+          name="postcode"
+          label="Postcode"
+          rules={[
+            {
+              required: true,
+              message: "Please input postcode!",
+            },
+          ]}
+        >
+          <Input className="w-full" placeholder="Postcode" />
+        </Form.Item>
+        <Form.Item
+          name="landmark"
+          label="Landmark"
+          rules={[
+            {
+              required: true,
+              message: "Please input landmark!",
+            },
+          ]}
+        >
+          <Input placeholder="Landmark" />
+        </Form.Item>
+        <Form.Item
+          name="address"
+          label="Address"
+          rules={[
+            {
+              required: true,
+              message: "Please input address!",
+            },
+          ]}
+          className="col-span-1 lg:col-span-3"
+        >
+          <Input.TextArea rows={6} placeholder="Address" />
+        </Form.Item>
+      </div>
       <div className="flex justify-end gap-5 mt-7">
         <Button
           disabled={currentStep === 0}
@@ -18,10 +77,10 @@ export const Location = ({
         >
           Back
         </Button>
-        <Button type="primary" onClick={() => setCurrentStep(currentStep + 1)}>
+        <Button htmlType="submit" type="primary">
           Next
         </Button>
       </div>
-    </div>
+    </Form>
   );
 };
