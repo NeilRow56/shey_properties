18b1d2d1401d18968a1bd44e1e09d08094c56ae5
NeilRow56 <neilrowland56@gmail.com>
2024-07-15 17:59:04 +0100

    Added edit and delete images functionality to media.tsx

--- src/app/site/user/properties/_components/property-form/media.tsx
+++ src/app/site/user/properties/_components/property-form/media.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React, { useState } from "react";
 import { PropertiesFormStepProps } from ".";
 import { Button, Upload } from "antd";
@@ -21,6 +22,38 @@ export const Media = ({
   };
   return (
     <div>
+      <div className="flex flex-wrap gap-5 mb-5">
+        {finalValues.media.images.map((image: string, index: string) => (
+          <div
+            key={index}
+            className="flex flex-col gap-3 border border-dashed border-gray-400 p-2 rounded justify-center items-center"
+          >
+            <img
+              src={image}
+              alt=""
+              className="w-20 h-20 object-cover rounded"
+            />
+            <span
+              className="text-red-500 underline text-sm cursor-pointer"
+              onClick={() => {
+                let tempMedia = finalValues.media;
+                tempMedia.images = tempMedia.images.filter(
+                  (img: string) => img !== image
+                );
+                setFinalValues({
+                  ...finalValues,
+                  media: {
+                    newlyUploadedFiles: tempFiles,
+                    images: tempMedia.images,
+                  },
+                });
+              }}
+            >
+              Delete
+            </span>
+          </div>
+        ))}
+      </div>
       <Upload
         listType="picture-card"
         multiple
