4afd8e10bfddf711675d9c1eb79447645912b64f
NeilRow56 <neilrowland56@gmail.com>
2024-07-16 09:03:48 +0100

    Add condition to only show current logged in user data in properties table

--- src/app/site/user/properties/_components/properties-table.tsx
+++ src/app/site/user/properties/_components/properties-table.tsx
@@ -1,9 +1,14 @@
 import db from "@/lib/db";
 import React from "react";
 import { PropertiesTableClientide } from "./properties-table-client";
+import { GetCurrentUserFromMongoDB } from "@/actions/user";
 
 export const PropertiesTable = async () => {
+  const user = await GetCurrentUserFromMongoDB();
   const properties = await db.property.findMany({
+    where: {
+      userId: user.data?.id,
+    },
     orderBy: {
       updatedAt: "desc",
     },
