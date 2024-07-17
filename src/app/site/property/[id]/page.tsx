1dea4a61145963eed937a82bd4c8c81aeafc4edf
NeilRow56 <neilrowland56@gmail.com>
2024-07-17 05:31:03 +0100

    Added 2nd stage of Individual Property Page

--- src/app/site/property/[id]/page.tsx
+++ src/app/site/property/[id]/page.tsx
@@ -17,6 +17,29 @@ const IndividualPropertyPage = async ({ params: { id } }: Props) => {
       id: id,
     },
   })) as Property;
+
+  const getAttributeDetails = ({
+    name,
+    value,
+  }: {
+    name: string;
+    value: any;
+  }) => {
+    return (
+      <div className="flex justify-between">
+        <span className="text-sm text-gray-600">{name}</span>
+        <span className="text-sm text-gray-600">{value}</span>
+      </div>
+    );
+  };
+
+  const getSectionTitle = (title: string) => (
+    <div>
+      <h1 className="text-xl font-bold text-gray-700">{title}</h1>
+      <hr className="border border-solid border-gray-300" />
+    </div>
+  );
+
   return (
     <div className="mt-12">
       <LinkButton title="Back to Properties" href="/" />
@@ -41,7 +64,56 @@ const IndividualPropertyPage = async ({ params: { id } }: Props) => {
 
           <p className="text-sm text-gray-600 mt-7">{property.description}</p>
         </div>
-        <div></div>
+        <div className="border border-solid border-gray-300 rounded p-5">
+          <div className="flex flex-col gap-1">
+            {getSectionTitle("Amenities")}
+            {getAttributeDetails({
+              name: "Bedrooms",
+              value: property.bedrooms,
+            })}
+            {getAttributeDetails({
+              name: "Bathrooms",
+              value: property.bathrooms,
+            })}
+            {getAttributeDetails({ name: "Parking", value: property.parking })}
+            {getAttributeDetails({ name: "Area", value: property.area })}
+            {getAttributeDetails({ name: "Parking", value: property.parking })}
+            {getAttributeDetails({
+              name: "Furnishing",
+              value: property.furnishing,
+            })}
+            {getAttributeDetails({ name: "Floors", value: property.floors })}
+            {getAttributeDetails({ name: "Age", value: property.age })}
+          </div>
+
+          <div className="flex flex-col gap-1 mt-7">
+            {getSectionTitle("Address")}
+            {getAttributeDetails({ name: "City", value: property.city })}
+            {getAttributeDetails({
+              name: "Landmark",
+              value: property.landmark,
+            })}
+            {getAttributeDetails({ name: "Zipcode", value: property.pincode })}
+            {getAttributeDetails({ name: "Address", value: property.address })}
+          </div>
+          {property.showOwnerContact && (
+            <div className="flex flex-col gap-1 mt-7">
+              {getSectionTitle("Owner Details")}
+              {getAttributeDetails({
+                name: "Owner name",
+                value: property.ownerName,
+              })}
+              {getAttributeDetails({
+                name: "Email",
+                value: property.ownerEmail,
+              })}
+              {getAttributeDetails({
+                name: "Phone",
+                value: property.ownerPhone,
+              })}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
