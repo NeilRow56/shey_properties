981fc0cc878934152d43bf68f0dcb3390ccfc966
NeilRow56 <neilrowland56@gmail.com>
2024-07-17 06:08:03 +0100

    UI for query modal complete

--- src/app/site/property/_components/query-modal.tsx
+++ src/app/site/property/_components/query-modal.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import { Button, Form, Modal } from "antd";
+import { Button, Form, Input, InputNumber, Modal } from "antd";
 import React, { useState } from "react";
 
 const QueryModal = ({ propertyId }: { propertyId: string }) => {
   const [showQueryModal, setShowQueryModal] = useState(false);
   const [loading, setLoading] = useState(false);
+
+  const onFinish = async (values: any) => {};
 
   return (
     <div className="mt-7">
@@ -21,7 +23,56 @@ const QueryModal = ({ propertyId }: { propertyId: string }) => {
           width={600}
           footer={null}
         >
-          <Form>Form</Form>
+          <Form
+            layout="vertical"
+            name="query-form"
+            onFinish={onFinish}
+            className="flex flex-col gap-5"
+          >
+            <Form.Item
+              name="name"
+              label="Name"
+              rules={[{ required: true, message: "Please enter your name" }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="quoteAmount"
+              label="Quote Amount"
+              rules={[
+                { required: true, message: "Please enter your quote amount" },
+              ]}
+            >
+              <InputNumber className="w-full" />
+            </Form.Item>
+            <Form.Item
+              name="message"
+              label="Message"
+              rules={[{ required: true, message: "Please enter your message" }]}
+            >
+              <Input.TextArea rows={2} />
+            </Form.Item>
+            <Form.Item
+              name="phoneNumber"
+              label="Phone Number"
+              rules={[{ required: true, message: "Please enter your phone" }]}
+            >
+              <Input />
+            </Form.Item>
+
+            <div className="flex justify-end gap-5">
+              <Button
+                htmlType="button"
+                onClick={() => setShowQueryModal(false)}
+                disabled={loading}
+              >
+                Cancel
+              </Button>
+              <Button type="primary" htmlType="submit" loading={loading}>
+                Send
+              </Button>
+            </div>
+          </Form>
         </Modal>
       )}
     </div>
